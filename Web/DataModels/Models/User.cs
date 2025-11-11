edd2e17f6960785efbec7636cf52a9d1f3e29294
frog24 <dlavsakogogovna228@gmail.com>
2025-11-11 23:04:59 +0400

    Веб-страницы входа и регистрации

---
+++ Web/DataModels/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModels.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string PasswordHash { get; set; }
+    }
+}
