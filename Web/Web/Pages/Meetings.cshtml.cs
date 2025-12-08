bffd783c9c3c24088822355d40e4b9c6ab245d10
frog24 <dlavsakogogovna228@gmail.com>
2025-12-08 04:02:29 +0400

    поиск по мероприятиям на странице мероприятий

--- Web/Web/Pages/Meetings.cshtml.cs
+++ Web/Web/Pages/Meetings.cshtml.cs
@@ -8,11 +8,18 @@ namespace Web.Pages
     public class MeetingsModel : PageModel
     {
         public List<MeetingViewModel> Meetings { get; set; } = new List<MeetingViewModel>();
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; } = "";
         public async Task OnGet()
         {
             try
             {
                 Meetings = await APIClient.GetAsync<List<MeetingViewModel>>("api/meeting/getlist");
+                if (SearchString != null && !string.IsNullOrEmpty(SearchString))
+                {
+                    Meetings = Meetings.Where(m => m.Title.Contains(SearchString, StringComparison.OrdinalIgnoreCase) 
+                    || m.Description.Contains(SearchString, StringComparison.OrdinalIgnoreCase)).ToList();
+                }
             }
             catch
             {
